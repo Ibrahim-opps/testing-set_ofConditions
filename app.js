0d407bccc84c533a663d53b84581d29ad203a1a9
Ibrahim_opps <ibrahim.hasan00710@gmail.com>
2025-07-03 22:35:34 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 let ch=prompt("Enter any character(1,G,s,number lowercase letter or uppercase letter)")
-if(ch>=0){
 if(ch.length === 0){
 console.log("Space cannot be blank");
-}}else{
+}else{
     if(ch>=0){
         console.log(`'${ch}' is a number.`);        
     }
