c0631c8c50b2841faa4455f866174d30e9d977c3
Ibrahim_opps <ibrahim.hasan00710@gmail.com>
2025-07-03 22:46:57 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-let ch=prompt("Enter any character(1,G,s,number lowercase letter or uppercase letter)")
-if(ch.length === 0){
-console.log("Space cannot be blank");
-}else{
-    if(ch>=0){
-        console.log(`'${ch}' is a number.`);        
-    }
-}if (ch.length !== 1&&ch >= 'A' && ch <= 'Z') {
-        console.log(`'${ch}' is an uppercase letter.`);
-    } else if (ch >= 'a' && ch <= 'z') {
-        console.log(`'${ch}' is a lowercase letter.`);
-    }else{
-        
-    }
-    
+let ch = prompt("Enter any one character (number,upercase letter or lowercase letter)");
+if (ch.length === 0) {
+    console.log("Space cannot be blank");
+    alert("Space cannot be blank");
+} 
+else if (!isNaN(ch)) {
+    console.log(`'${ch}' is a number.`);
+} 
+else if (ch.length === 1 && ch >= 'A' && ch <= 'Z') {
+    console.log(`'${ch}' is an uppercase letter.`);
+} 
+else if (ch.length === 1 && ch >= 'a' && ch <= 'z') {
+    console.log(`'${ch}' is a lowercase letter.`);
+} 
+else {
+    console.log("Invalid input! Please enter one single letter.");
+}
